6a3b38ae87d4a8b2b6e7d9db0cd7ffb937acecdf
João Henrique <joaohen.t8@gmail.com>
2025-05-22 16:57:33 -0300

    Add files via upload

---
+++ php/conexao.php
@@ -0,0 +1,11 @@
+<?php
+$server = "localhost";
+$user = "root";
+$password = "";
+$database = "locadora";
+
+$conexao = new mysqli($server, $user, $password, $database);
+
+
+
+?>
---
+++ php/register.php
@@ -0,0 +1,9 @@
+<?php
+if(isset($_POST['loginBtn'])){
+    $userEmail = $_POST['email'];
+    $useerName = $_POST['userName'];
+    $userPassword = $_POST['password'];
+}
+
+
+?>
